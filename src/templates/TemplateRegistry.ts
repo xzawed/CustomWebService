f413c2a2750d376a6b1dcbc88de2d2096f8080c4
xzawed31 <xzawed31@gmail.com>
2026-04-12 21:14:21 +0900

    feat: TemplateRegistry에 11개 템플릿 전체 등록

--- src/templates/TemplateRegistry.ts
+++ src/templates/TemplateRegistry.ts
@@ -3,6 +3,14 @@ import type { ApiCatalogItem } from '@/types/api';
 import { DashboardTemplate } from './DashboardTemplate';
 import { CalculatorTemplate } from './CalculatorTemplate';
 import { GalleryTemplate } from './GalleryTemplate';
+import { InfoLookupTemplate } from './InfoLookupTemplate';
+import { MapServiceTemplate } from './MapServiceTemplate';
+import { ContentFeedTemplate } from './ContentFeedTemplate';
+import { ComparisonTemplate } from './ComparisonTemplate';
+import { TimelineTemplate } from './TimelineTemplate';
+import { NewsCuratorTemplate } from './NewsCuratorTemplate';
+import { QuizTemplate } from './QuizTemplate';
+import { ProfileTemplate } from './ProfileTemplate';
 
 class TemplateRegistryImpl {
   private templates = new Map<string, ICodeTemplate>();
@@ -11,6 +19,14 @@ class TemplateRegistryImpl {
     this.register(new DashboardTemplate());
     this.register(new CalculatorTemplate());
     this.register(new GalleryTemplate());
+    this.register(new InfoLookupTemplate());
+    this.register(new MapServiceTemplate());
+    this.register(new ContentFeedTemplate());
+    this.register(new ComparisonTemplate());
+    this.register(new TimelineTemplate());
+    this.register(new NewsCuratorTemplate());
+    this.register(new QuizTemplate());
+    this.register(new ProfileTemplate());
   }
 
   register(template: ICodeTemplate): void {
